65f6c02200fa32c95e0ad56e596e805cd5752dc5
Mark Penaranda <macbookpro@MacBooks-MacBook-Pro.local>
2014-08-07 17:20:51 +0800

    Employee Fix

--- application/models/eloquent/Employee.php
+++ application/models/eloquent/Employee.php
@@ -21,21 +21,23 @@ class Employee extends Eloquent {
                       'birthdate',
                       'gender',
                       'marital_status',
+                      'spouse_name',
+                      
 
                       // Employee Details 
 
                       'employee_type',
-                      'payroll_period'
+                      'payroll_period',
                       'job_position',
                       'department',
                       'role_id',
                       'branch_id',
                       'date_hired',
                       'date_ended',
+                      'basic_pay',
 
                       // Government Details
 
-                      'basic_pay',
                       'tin_number',
                       'sss_number',
                       'pagibig_number',
@@ -47,18 +49,70 @@ class Employee extends Eloquent {
                       'profile_picture',
                       'fb',
                       'email', 
-                      
+
                 ];
 
-
+ public function getDateHired()
+ {
+  return $this->date_hired;
+ }
  public function getName()
  {
     return $this->first_name .' ' . $this->middle_name . ' ' . $this->last_name;
  }               
+ public function getFullAddress()
+ {
+  return $this->full_address;
+ }
+ public function getBirthdate()
+ {
+  return $this->birthdate;
+ }
+ public function getGender()
+ {
+  return $this->gender;
+ }
+ public function getMaritalStatus()
+ {
+  return $this->marital_status;
+ }
+// Employee Details
+ public function getEmployeeType()
+ {
+   return $this->employee_type;
+ }
+ public function getPayrollPeriod()
+ {  
+  return $this->payroll_period;
+ }
+
  public function getJobPosition()
  {
     return Job_Position::find($this->job_position)->job_position;
  }
+  public function getDepartment()
+ {
+    return Department::find($this->department)->department_name;
+ }
+ public function getDateEnded()
+ {
+   return $this->date_ended;
+ }
+ public function getTin()
+ {
+  return $this->tin_number;
+ }
+  public function getSSS()
+ {
+
+  return $this->sss_number;
+ }
+
+ public function getPagibig()
+ {
+  return $this->pagibig_number;
+ }
+
  public function getProfilePicture()
  {
   return '/media?image=' . $this->profile_picture;
@@ -66,11 +120,6 @@ class Employee extends Eloquent {
  public function getBranch()
  {
    return Branch::find($this->branch_id)->branch_name;
- }
-
- public function getPayrollPeriod()
- {
-  return 'Monthly';
  }
 
 }
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,47 @@
-$('#create_user').on('click', function(){
-	if( $(this).is(':checked') ) {
-		$('.user-account-form').fadeIn();		
-	}
-	else{
-		$('.user-account-form').fadeOut();
-	}
+$(document).ready(function(){
+	$('#create_user').on('click', function(){
+		if( $(this).is(':checked') ) {
+			$('.user-account-form').fadeIn();		
+		}
+		else{
+			$('.user-account-form').fadeOut();
+		}
+	});
+
+
+	$('.basic-information .edit-profile-btn').on('click', function(){
+		$('.basic-information .profile-value').fadeOut('fast');
+		$('.basic-information .edit-input').fadeIn();
+		$('.basic-information .save-cancel-btn').fadeIn();
+	});
+	$('.basic-information .btn-cancel').on('click', function(){
+	
+		$('.basic-information .edit-input').fadeOut('fast');
+		$('.basic-information .save-cancel-btn').fadeOut('fast');
+		$('.basic-information .profile-value').fadeIn();
+	});
+
+	$('.employment-details .edit-profile-btn').on('click', function(){
+		$('.employment-details .profile-value').fadeOut('fast');
+		$('.employment-details .edit-input').fadeIn();
+		$('.employment-details .save-cancel-btn').fadeIn();
+	});
+	$('.employment-details .btn-cancel').on('click', function(){
+	
+		$('.employment-details .edit-input').fadeOut('fast');
+		$('.employment-details .save-cancel-btn').fadeOut('fast');
+		$('.employment-details .profile-value').fadeIn();
+	});
+
+	$('.government-information .edit-profile-btn').on('click', function(){
+		$('.government-information .profile-value').fadeOut('fast');
+		$('.government-information .edit-input').fadeIn();
+		$('.government-information .save-cancel-btn').fadeIn();
+	});
+	$('.government-information .btn-cancel').on('click', function(){
+	
+		$('.government-information .edit-input').fadeOut('fast');
+		$('.government-information .save-cancel-btn').fadeOut('fast');
+		$('.government-information .profile-value').fadeIn();
+	});
 });
