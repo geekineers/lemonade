a4e09de071198af8c58b291b54e43e9cce40204d
Mark Penaranda <markangelpenaranda@gmail.com>
2014-08-28 14:00:40 +0800

    Merge

---
+++ application/migrations/030_add_form_companyid.php
@@ -0,0 +1,32 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Add_form_companyid extends CI_Migration
+{
+
+    public function up()
+    {
+        // $this->dbforge->drop_table('branches');
+        $this->dbforge->add_column('form_application', array(
+                'company_id' => array(
+                    'type'       => 'INT',
+                    'constraint' => 5,
+                ),
+                
+
+                ));
+    
+    
+    
+    
+    
+
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('branches');
+    }
+}
+
