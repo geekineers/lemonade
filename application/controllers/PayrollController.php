66411f5e921844637dcbf84417035a0075b06fb2
Mark Penaranda <markangelpenaranda@gmail.com>
2014-09-15 11:05:56 +0800

    Layout Payslip

--- application/controllers/PayrollController.php
+++ application/controllers/PayrollController.php
@@ -3,167 +3,153 @@ defined('BASEPATH') or exit('No direct script access allowed');
 require_once ('BaseController.php');
 
 // use Maatwebsite\Excel\Excel as Excel;
-class PayrollController extends BaseController
-{
+class PayrollController extends BaseController {
 
-    protected $branchesRepository, $payslipsGroupRepository, $payslipsRepository, $employeeRepository, $payrollGroupRepository;
-    public function __construct()
-    {
-        parent::__construct();
-        $this->mustBeLoggedIn();
-        $this->branchesRepository      = new BranchRepository();
-        $this->payslipsRepository      = new PayslipsRepository();
-        $this->payrollGroupRepository  = new PayrollGroupRepository();
-        $this->employeeRepository      = new EmployeeRepository();
-        $this->payslipsGroupRepository = new PayslipsGroupRepository();
-    }
+	protected $branchesRepository, $payslipsGroupRepository, $payslipsRepository, $employeeRepository, $payrollGroupRepository;
+	public function __construct() {
+		parent::__construct();
+		$this->mustBeLoggedIn();
+		$this->branchesRepository      = new BranchRepository();
+		$this->payslipsRepository      = new PayslipsRepository();
+		$this->payrollGroupRepository  = new PayrollGroupRepository();
+		$this->employeeRepository      = new EmployeeRepository();
+		$this->payslipsGroupRepository = new PayslipsGroupRepository();
+	}
 // GET
-    public function index()
-    {
+	public function index() {
 
-        $data['user']          = $this->employeeRepository->getLoginUser($this->sentry->getUser());
-        $data['company']       = $this->company;
-        $data['title']         = 'Payroll Generation';
-        $data['payslipGroups'] = $this->payslipsGroupRepository->all();
-        $data['payrollgroups'] = $this->payrollGroupRepository->all();
-        $data['branches']      = $this->branchesRepository->all();
+		$data['user']          = $this->employeeRepository->getLoginUser($this->sentry->getUser());
+		$data['company']       = $this->company;
+		$data['title']         = 'Payroll Generation';
+		$data['payslipGroups'] = $this->payslipsGroupRepository->all();
+		$data['payrollgroups'] = $this->payrollGroupRepository->all();
+		$data['branches']      = $this->branchesRepository->all();
 
-        $this->render('payroll/index.twig.html', $data);
-    }
+		$this->render('payroll/index.twig.html', $data);
+	}
 
-    public function restGetPayrollGroup()
-    {
-        $id   = $this->input->get('id');
-        $data = $this->payrollGroupRepository->getPayrollGroupByBranch($id);
-        return $this->sendJSON($data);
-    }
-    public function masterList($id)
-    {
-        $from = $this->input->get('from');
-        $to   = $this->input->get('to');
-        $slip = $this->payslipsGroupRepository->getPayslipById($id, $from, $to)->getAllPayslips();
-        // dd($slip);
-        $company = $this->company;
-        $data = [
-            'payslips' => $slip,
-            'from'     => $from,
-            'to'       => $to,
-            'period'   => $this->payslipsGroupRepository->getPayslipById($id, $from, $to),
-   		   'company_logo' => $company->company_logo
-        ];
-        $html = $this->load->view('payroll/masterlist', $data, true);
-        // dd($html);
-        // $html = "dsadas";
-        // dd($html);
-        // echo $html;
-        $pdf = pdf_create($html, '', false, true);
-        echo $pdf;
-    }
+	public function restGetPayrollGroup() {
+		$id   = $this->input->get('id');
+		$data = $this->payrollGroupRepository->getPayrollGroupByBranch($id);
+		return $this->sendJSON($data);
+	}
+	public function masterList($id) {
+		$from = $this->input->get('from');
+		$to   = $this->input->get('to');
+		$slip = $this->payslipsGroupRepository->getPayslipById($id, $from, $to)->getAllPayslips();
+		// dd($slip);
+		$company = $this->company;
+		$data    = [
+			'payslips'     => $slip,
+			'from'         => $from,
+			'to'           => $to,
+			'period'       => $this->payslipsGroupRepository->getPayslipById($id, $from, $to),
+			'company_logo' => $company->company_logo
+		];
+		$html = $this->load->view('payroll/masterlist', $data, true);
+		// dd($html);
+		// $html = "dsadas";
+		// dd($html);
+		// echo $html;
+		$pdf = pdf_create($html, '', false, true);
+		echo $pdf;
+	}
 
-    public function groupList($id)
-    {
+	public function groupList($id) {
 
-        $from         = $this->input->get('from');
-        $to           = $this->input->get('to');
-        $data['user'] = $this->employeeRepository->getLoginUser($this->sentry->getUser());
-        // dd($this->payrollGroupRepository->getDate($id));
+		$from         = $this->input->get('from');
+		$to           = $this->input->get('to');
+		$data['user'] = $this->employeeRepository->getLoginUser($this->sentry->getUser());
+		// dd($this->payrollGroupRepository->getDate($id));
 
-        $data['id']       = $id;
-        $data['from']     = $from;
-        $data['to']       = $to;
-        $data['payslips'] = $this->payslipsGroupRepository->getPayslipById($id, $from, $to)->getAllPayslips();
-        $data['company']  = $this->company;
-        $this->render('payroll/group-slip.twig.html', $data);
+		$data['id']       = $id;
+		$data['from']     = $from;
+		$data['to']       = $to;
+		$data['payslips'] = $this->payslipsGroupRepository->getPayslipById($id, $from, $to)->getAllPayslips();
+		$data['company']  = $this->company;
+		$this->render('payroll/group-slip.twig.html', $data);
 
-    }
+	}
 // GET
-    public function payslip()
-    {
-        $data['user'] = $this->employeeRepository->getLoginUser($this->sentry->getUser());
+	public function payslip() {
+		$data['user'] = $this->employeeRepository->getLoginUser($this->sentry->getUser());
 
-        $data['title'] = 'Payroll Generation';
+		$data['title'] = 'Payroll Generation';
 
-        $data['payslips'] = $this->payslipsRepository->getAllPayslip();
-        $data['company']  = $this->company;
-        $this->render('payroll/payslip.twig.html', $data);
+		$data['payslips'] = $this->payslipsRepository->getAllPayslip();
+		$data['company']  = $this->company;
+		$this->render('payroll/payslip.twig.html', $data);
 
-    }
-    public function slip($id)
-    {
+	}
+	public function slip($id) {
 
-        $slip = $this->payslipsRepository->getSlipById($id);
-        $company = $this->company;
+		$slip    = $this->payslipsRepository->getSlipById($id);
+		$company = $this->company;
 
-        $data = [
-            'employee' => $slip->getEmployee(),
-            'payslip'  => $slip,
-            'company' => $company,
-          
-        ];
+		$data = [
+			'employee' => $slip->getEmployee(),
+			'payslip'  => $slip,
+			'company'  => $company,
 
-        // dd($data);
-        $html            = $this->load->view('payroll/payslip_template', $data, true);
-        // echo $html;
-        $pdf = pdf_create($html, '', false, true);
-        echo $pdf;
+		];
 
-    }
+		// dd($data);
+		$html = $this->load->view('payroll/payslip_template', $data, true);
+		// echo $html;
+		$pdf = pdf_create($html, '', false, true);
+		echo $pdf;
+
+	}
 
 // POST
-    public function generatePayslip()
-    {
+	public function generatePayslip() {
 
-        $this->load->helper(array('dompdf', 'file'));
-        $data        = $this->input->post();
-        $prepared_by = $this->employeeRepository->getLoginUser($this->sentry->getUser())->id;
-        echo $this->payslipsRepository->generatePayslip($data, $prepared_by);
+		$this->load->helper(array('dompdf', 'file'));
+		$data        = $this->input->post();
+		$prepared_by = $this->employeeRepository->getLoginUser($this->sentry->getUser())->id;
+		echo $this->payslipsRepository->generatePayslip($data, $prepared_by);
 
-    }
-    public function deletePayslips()
-    {
-        $id = $this->input->post('id');
-        $this->payslipsGroupRepository->deletePayslips($id);
-    }
+	}
+	public function deletePayslips() {
+		$id = $this->input->post('id');
+		$this->payslipsGroupRepository->deletePayslips($id);
+	}
 // GET
-    public function bank()
-    {
-        $data['user'] = $this->employeeRepository->getLoginUser($this->sentry->getUser());
+	public function bank() {
+		$data['user'] = $this->employeeRepository->getLoginUser($this->sentry->getUser());
 
-        $data['title'] = 'Payroll Generation';
-        $this->render('payroll/govform.twig.html', $data);
-    }
+		$data['title'] = 'Payroll Generation';
+		$this->render('payroll/govform.twig.html', $data);
+	}
 //
-    public function myPaySlips()
-    {
-        $data['company']  = $this->company;
-        $data['user']     = $this->employeeRepository->getLoginUser($this->sentry->getUser());
-        $data['payslips'] = $this->payslipsRepository->where('employee_id', '=', $data['user']->id)->get();
-        $data['title']    = 'My Payslips';
-        $this->render('payroll/mypayslip.twig.html', $data);
+	public function myPaySlips() {
+		$data['company']  = $this->company;
+		$data['user']     = $this->employeeRepository->getLoginUser($this->sentry->getUser());
+		$data['payslips'] = $this->payslipsRepository->where('employee_id', '=', $data['user']->id)->get();
+		$data['title']    = 'My Payslips';
+		$this->render('payroll/mypayslip.twig.html', $data);
 
-    }
+	}
 
 // GET
-    public function govform($id)
-    {
-        $form = $this->input->get('form');
-        $from = $this->input->get('from');
-        $to   = $this->input->get('to');
-        $this->payslipsRepository->generateGovermentForms($id, $form, $from, $to);
-    }
+	public function govform($id) {
+		$form = $this->input->get('form');
+		$from = $this->input->get('from');
+		$to   = $this->input->get('to');
+		$this->payslipsRepository->generateGovermentForms($id, $form, $from, $to);
+	}
 
-    public function masterListInXls($id)
-    {
-        $from = $this->input->get('from');
-        $to   = $this->input->get('to');
-        $slip = $this->payslipsGroupRepository->getPayslipById($id, $from, $to)->getAllPayslips();
+	public function masterListInXls($id) {
+		$from = $this->input->get('from');
+		$to   = $this->input->get('to');
+		$slip = $this->payslipsGroupRepository->getPayslipById($id, $from, $to)->getAllPayslips();
 
-        $data = [
-            'payslips' => $slip,
-            'from'     => $from,
-            'to'       => $to
-        ];
+		$data = [
+			'payslips' => $slip,
+			'from'     => $from,
+			'to'       => $to
+		];
 
-    }
+	}
 
 }
--- application/views/payroll/payslip_template.php
+++ application/views/payroll/payslip_template.php
@@ -7,7 +7,7 @@
       <style>
         table {
   width:100%;
-    
+
 }
 table thead {
   background-color:#DBDBDB;
@@ -56,186 +56,209 @@ background-color:#F2F2F2;
       <table>
         <tr>
           <td>
-            <h3><img src="../uploads/<?php echo $company->company_logo; ?>" style="height:70px;"> </h3>
+            <h3><img src="../uploads/<?php echo $company->company_logo;?>" style="height:70px;"> </h3>
           </td>
           <td>
             <div class="address">
               <p>
-                <?php echo $company->company_address; ?>
-              </p>
+<?php echo $company->company_address;?>
+</p>
             </div>
-            
+
           </td>
         </tr>
       </table>
-      <?php
-        $from = $payslip->getPayslipsGroup()->from;
+<?php
+$from = $payslip->getPayslipsGroup()->from;
 
-        $to = $payslip->getPayslipsGroup()->to;
-      ?>
+$to = $payslip->getPayslipsGroup()->to;
+?>
       <table style="font-size:12px">
-      
-        
+
+
         <tr>
           <td>EMP #</td>
-          <td><?php echo $employee->id; ?></td>
+          <td><?php echo $employee->id;?></td>
           <td>PAYROLL PERIOD</td>
-          <td><?php echo $payslip->getPayslipsGroup()->from.'-'.$payslip->getPayslipsGroup()->to; ?></td>
+          <td><?php echo $payslip->getPayslipsGroup()->from . '-' . $payslip->getPayslipsGroup()->to;?></td>
 
         </tr>
         <tr>
           <td>NAME: </td>
-          <td><?php echo $employee->first_name; ?> <?php echo $employee->last_name; ?> <?php echo $employee->middle_name; ?></td>
+          <td><?php echo $employee->first_name;
+?> <?php echo $employee->last_name;
+?> <?php echo $employee->middle_name;
+?></td>
           <td>PAYROLL DATE</td>
           <td><?php echo $payslip->created_at?></td>
         </tr>
         <tr>
           <td>DESIGNATION</td>
-          <td><?php echo $employee->getJobPosition(); ?></td>
+          <td><?php echo $employee->getJobPosition();?></td>
           <td>PAYMENT MODE</td>
-          <td><?php echo $payslip->getPayslipsGroup()->getPayrollGroup()->period ?></td>
+          <td><?php echo $payslip->getPayslipsGroup()->getPayrollGroup()->period?></td>
         </tr>
-        
+
       </table>
 
       <div style="height: 10px; margin:10px 0px; background-color:black"></div>
 
-
-
-<div style="max-width:49%;position:absolute;left:300px">
-  <table class="bordered">
-     <thead>
-        <tr >
-          <td  colspan="2">DEDUCTION</td>
-        </tr>
-      </thead>
-
-      <!-- foreach -->
-        <?php foreach ($employee->getDeductions() as $deduction) { ?>
-          <tr>
-            <td><?php echo $deduction->getName(); ?></td>
-            <td><?php echo $deduction->getAmount(); ?></td>
-          </tr>
-        <?php } ?>
-
-        <tr>
-          <td>Late Deduction</td>
-          <td><?php echo $employee->getUnderTimeAndLateDeduction($from, $to, 'minute',true); ?></td>
-        </tr>
-      
-         <tr>
-          <td>Absent</td>
-          <td><?php echo $employee->getAbsentDeduction($from, $to,false,true); ?></td>
-        </tr>
-        <tr>
-          <td>SSS</td>
-          <td><?php echo $employee->getSSSValue()?></td>
-        </tr>
-        <tr>
-          <td>HDMF</td>
-          <td><?php echo number_format($payslip->pagibig,2) ?></td>
-        </tr>
-        <tr>
-          <td>Philhealth</td>
-          <td><?php echo number_format($payslip->philhealth,2) ?></td>
-        </tr>
-        <tr>
-          <td>Withholding Tax</td>
-          <td><?php echo $employee->getWithholdingTax($from, $to)?></td>
-        </tr>
-        <tr>
-          <td><b style="padding:15px ;">Total Deduction</b></td>
-          
-          <td><?php echo $employee->getAllandTotalDeduction($from, $to)?></td> 
-        </tr>
-        
-
-      <!-- end -->
-  </table>
-
-</div>
-<div style="max-width:49%;left:0">
+<div style="width:49%;display: inline-block;">
    <table class="bordered" >
-        <thead>
-          <tr >
+      <thead>
+          <tr>
             <td  colspan="2">INCOME</td>
           </tr>
-        </thead>
+      </thead>
+
+
 
         <!-- foreach -->
           <tr>
             <td>Basic Salary</td>
-            <td><?php echo $employee->getBasicSalary(true); ?></td>
+            <td style="text-align:right;"><?php echo $employee->getBasicSalary(true);
+?></td>
           </tr>
-
-         <?php foreach ($employee->getAllowances() as $allowance) { ?>
-            <tr>
-              <td><?php echo $allowance->getName(); ?></td>
-              <td><?php echo $allowance->getAmount(); ?></td>
-            </tr>
-             
-         
-         <?php } ?>
-          <tr>
+           <tr>
             <td >Overtime</td>
-            <td>
-              <?php echo $employee->getOvertime($from,$to)  ?>
-            </td>
+            <td style="text-align:right;">
+<?php echo $employee->getOvertime($from, $to)?>
+</td>
           </tr>
+<?php if (count($employee->getAllowances())) {?>
+	<tr>
+																								                        <td>Allowances</td>
+																								                        <td></td>
+																								                      </tr>
+	<?php }?>
+<?php foreach ($employee->getAllowances() as $allowance) {?>
+																								                                                <tr>
+																								                                                  <td style="text-align: right;"><?php echo $allowance->getName();
+	?></td>
+																								                                                  <td style="text-align: right;"><?php echo $allowance->getAmount(true);
+	?></td>
+																								                                                </tr>
 
-         
-          
-          <tr>
-            <td>Total Allowances</td>
-            <td>
-              <?php echo $employee->getTotalAllowances() ?>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
 
-          <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+	<?php }?>
+<tr><td>&nbsp;
+</td><td>&nbsp;
+</td></tr>
 
-          <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+          <tr><td>&nbsp;
+</td><td>&nbsp;
+</td></tr>
+
+          <tr><td>&nbsp;
+</td><td>&nbsp;
+</td></tr>
           <tr>
             <td ><b style="padding:15px ;">Gross Pay</b></td>
-            <td>
-              <?php echo  $payslip->getEmployee()->getGross($from,$to,true) ?>
+            <td style="text-align:right;">
+<?php echo $payslip->getEmployee()->getGross($from, $to, true)?>
             </td>
           </tr>
         <!-- endforeach -->
    </table>
 </div>
 
-    <table style="width:100%;position:absolute;top:500px;">
+<div style="width:49%; display: inline-block; left:300px">
+  <table class="bordered">
+     <thead>
+        <tr >
+          <td  colspan="2">DEDUCTION</td>
+        </tr>
+      </thead>
+         <tr>
+          <td>Withholding Tax</td>
+          <td style="text-align:right;"><?php echo $employee->getWithholdingTax($from, $to)?></td>
+        </tr>
+
+
+
+        <tr>
+          <td>SSS</td>
+          <td style="text-align:right;"><?php echo $employee->getSSSValue()?></td>
+        </tr>
+        <tr>
+          <td>HDMF</td>
+          <td style="text-align:right;"><?php echo number_format($payslip->pagibig, 2)?></td>
+        </tr>
+        <tr>
+          <td>Philhealth</td>
+          <td style="text-align:right;"><?php echo number_format($payslip->philhealth, 2)?></td>
+        </tr>
+
+         <tr>
+          <td>Absent</td>
+          <td style="text-align:right;"><?php echo $employee->getAbsentDeduction($from, $to, false, true);
+?></td>
+        </tr>
+         <tr>
+          <td>Late Deduction</td>
+          <td style="text-align:right;" ><?php echo $employee->getUnderTimeAndLateDeduction($from, $to, 'minute', true);
+?></td>
+        </tr>
+
+
+             <!-- foreach -->
+<?php if (count($employee->getDeductions())) {?>
+	<tr>
+																																	          <td>Others</td>
+																																	          <td></td>
+																																	        </tr>
+	<?php }?>
+<?php foreach ($employee->getDeductions() as $deduction) {?>
+																																										          <tr>
+																																										            <td style="text-align:right;" ><?php echo $deduction->getName();
+	?></td>
+																																										            <td style="text-align:right;" ><?php echo $deduction->getAmount(true);
+	?></td>
+																																										          </tr>
+	<?php }?>
+
+          <tr>
+          <td><b style="padding:15px ;">Total Deduction</b></td>
+
+          <td style="text-align:right;"><?php echo $employee->getAllandTotalDeduction($from, $to)?></td>
+        </tr>
+
+      <!-- end -->
+  </table>
+
+</div>
+
+
+    <table style="width:100%;top:150px;">
       <tr>
         <td style="background-color:black; color:white;">NET</td>
-        <td style="background-color:black; color:white;"><?php echo $employee->getNet($from, $to)?></td>
+        <td style="background-color:black; color:white; font-size:18px; text-align:right;" "><?php echo $employee->getNet($from, $to)?></td>
       </tr>
     </table>
 
        <!--  <tr >
           <td style="background-color:#E2FF3D !important" colspan="2">Net Pay</td>
-          <td style="background-color:#E2FF3D !important" colspan="2"><?php  //echo $payslip->net; ?></td>
+          <td style="background-color:#E2FF3D !important" colspan="2"><?php //echo $payslip->net; ?></td>
         </tr> -->
     <table class="bordered" style="margin-top:30px;background-color:#DBDBDB;position:absolute;bottom:200px;">
       <thead>
-         <tr>        
+         <tr>
            <td>Prepared By</td>
         </tr>
         <tr>
-          <td><?php echo $payslip->getPayslipsGroup()->getPreparedBy(); ?></td>
+          <td><?php echo $payslip->getPayslipsGroup()->getPreparedBy();?></td>
         </tr>
-      </thead>  
-     
-     </table
-      
-      
-     
-      
-      
-      
+      </thead>
 
-              
+     </table
+
+
+
+
+
+
+
+
   </div>
     </body>
 </html>
