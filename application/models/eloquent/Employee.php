e8b9b99d41ac77fa4be970ed24b97031f338af7b
Mark Penaranda <markangelpenaranda@gmail.com>
2014-12-23 06:03:53 +0800

    HDMF

--- application/models/eloquent/Employee.php
+++ application/models/eloquent/Employee.php
@@ -437,10 +437,10 @@ class Employee extends BaseModel
     public function getHDMFValue()
     {
         $hdmf = ($this->deduct_hdmf == null || $this->deduct_hdmf == 1)? 100 : 0;
+        $period = str_replace(" ", "", strtolower($this->getPayrollPeriod()->period));
 
         $hdmf = (strtolower($this->fixed_hdmf_amount) != "no") ? (int) $this->fixed_hdmf_amount : $hdmf;
-
-        if ($this->getPayrollPeriod()->period == "Semi-monthly") {return $hdmf / 2;
+        if ($period == "semi-monthly") {return $hdmf / 2;
         }
 
         return number_format($hdmf, 2);
