eff56461188a5b993bcaf5d4f0ee7fc9fcc27f1f
Mark Penaranda <markangelpenaranda@gmail.com>
2015-01-26 02:31:13 +0800

    fix dll

--- application/models/eloquent/Employee.php
+++ application/models/eloquent/Employee.php
@@ -855,7 +855,7 @@ class Employee extends BaseModel
                 $lunch_break = $this->getCompany()->company_lunch_break;
                 $min_hours_of_work = 480 - $lunch_break;
                 $undertime = $undertime - $lunch_break;
-                $undertime = ($undertime >= min_hours_of_work) ? min_hours_of_work : $undertime;
+                $undertime = ($undertime >= $min_hours_of_work) ? $min_hours_of_work : $undertime;
                 if(!$is_undertime_approved){
                     $totalUnderTime += $undertime;                    
                 }
