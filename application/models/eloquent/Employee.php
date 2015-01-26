f988a2776412af1addf9c52c6f945f3d52cef741
Mark Penaranda <markangelpenaranda@gmail.com>
2015-01-27 02:36:54 +0800

    M

--- application/models/eloquent/Employee.php
+++ application/models/eloquent/Employee.php
@@ -1037,13 +1037,13 @@ class Employee extends BaseModel
                 $ot_pay = $this->getCompany()->company_special_holiday_overtime_pay/100;
                 $ot_pay = $ot_pay + 1;
                 $special_holiday_rest_rate = $special_holiday_rest_rate + 1;
-                $overtime_pay = $ot_pay * $special_holiday_rest_rate * rest_rate;
+                $overtime_pay = $ot_pay * $special_holiday_rest_rate * $rest_rate;
                 break;
             case 'regular_holiday_rest_day':
                 $ot_pay = $this->getCompany()->company_regular_holiday_overtime_pay/100;
                 $ot_pay = $ot_pay + 1;
                 $regular_holiday_pay = $regular_holiday_rest_rate + 2;
-                $overtime_pay = $ot_pay * $regular_holiday_pay * rest_rate;
+                $overtime_pay = $ot_pay * $regular_holiday_pay * $rest_rate;
                 break;
             
             default:
