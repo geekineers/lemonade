724afcf8f2974fcca678cbdc278142134e62c8c7
Mark Penaranda <markangelpenaranda@gmail.com>
2015-01-26 17:39:01 +0800

    fix

--- application/models/eloquent/Employee.php
+++ application/models/eloquent/Employee.php
@@ -1336,7 +1336,7 @@ class Employee extends BaseModel
         $regular_holiday_attendance = $this->getRegularHolidayAttendance($from, $to, "not_rest_day_attendance");
         $special_holiday_attendance = $this->getSpecialHolidayAttendance($from, $to);
         if($type == "normal_day"){
-            return $this->getInAttendance($from, $to) - ( $this->getRegularHolidayAttendance($from, $to, "rest_day_attendance"); + $this->getRestDayAttendance($from, $to, 'not_holiday')); 
+            return $this->getInAttendance($from, $to) - ( $this->getRegularHolidayAttendance($from, $to, "rest_day_attendance") + $this->getRestDayAttendance($from, $to, 'not_holiday')); 
         }
         return $regular_holiday_attendance;
     }
